c455238d792441ea1826fc14949f8767fb03133c
qhung23125005 <xinquyhung91005@gmail.com>
2025-03-15 13:50:39 +0700

    added VGG16

---
+++ src/vgg16.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from activations import get_activation
+from initializers import initialize_weights
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name, activation, initializer, sample_input = None):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(activation, cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
+
+        # Initialize weights
+        self.apply(lambda m: initialize_weights(m, initializer, sample_input))
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, activation, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           get_activation(activation=activation)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+    
+def vgg16(activation, initializer, sample_input = None):
+    return VGG('VGG16', activation, initializer, sample_input)
